dbc999c6cbd2af345cd996015d982a003334ca9f
Nicolas Isnardi <nicolasisnardi@gmail.com>
2022-02-16 20:56:22 -0600

    Added initial layout and image

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import { View, Text, Image, StyleSheet } from 'react-native';
+import myPicture from './assets/me.jpg';
 
 const App = () => {
   return <View style={styles.container}>
-    <Text>Hi</Text>
-  </View>
+    <View style={styles.content}>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={myPicture} />
+      </View>
+      <View>
+        <Text>Your Name</Text>
+        <Text>Twitter Username</Text>
+        <Text>Linkeding Username</Text>
+        <Text>Instagram Username</Text>
+      </View>
+    </View>
+  </View >
 };
 
 const styles = StyleSheet.create({
@@ -13,6 +24,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
-})
+  content: {
+    flexDirection: 'row',
+  },
+  image: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+  },
+  imageContainer: {
+    shadowColor: '#171717',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+    elevation: 20,
+  },
+  // row: {
+  //   flexDirection: 'row',
+  //   alignItems: 'center',
+  //   marginBottom: 5,
+  // },
+});
 
 export default App;
+
+
+// flexDirection: 'row',
+//   width: '52%',
+//     justifyContent: 'space-between',
